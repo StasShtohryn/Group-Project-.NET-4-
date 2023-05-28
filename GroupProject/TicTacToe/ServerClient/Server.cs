dbdc9c819760de526b731d5c9519e719f4e42438
Stas Shtohryn <stasshtohryn2002@gmail.com>
2023-05-28 17:15:09 +0300

    Add Constructor

--- GroupProject/TicTacToe/ServerClient/Server.cs
+++ GroupProject/TicTacToe/ServerClient/Server.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,5 +11,10 @@ namespace TicTacToe.ServerClient
     public class Server
     {
         TcpListener listener;
+
+        public Server(IPEndPoint localEndPoint)
+        {
+            listener = new TcpListener(localEndPoint);
+        }
     }
 }
