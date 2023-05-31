224452de7ff184b53e3e1233c77f4ba824e3aed2
regularuser548 <regularuser548@tutanota.com>
2023-05-31 19:41:17 +0300

    Added static client

---
+++ GroupProject/TicTacToe/Model/StaticClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Model
+{
+    internal static class StaticClient
+    {
+        static TicTacToe.Client.Client Client;
+
+        public static void Init(string host, int port)
+        {
+            Client = new(host, port);
+        }
+
+        public static async Task Send(string message)
+        {
+            await Client.Send(message);
+        }
+
+        public static async Task<string> Recive()
+        {
+            return await Client.Recive();
+        }
+
+        public static void Dispose()
+        {
+            Client.Dispose();
+        }
+    }
+}
