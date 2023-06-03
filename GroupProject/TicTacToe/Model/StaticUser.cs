4f0ade4e961705df76a86be7f27feff01490e065
regularuser548 <regularuser548@tutanota.com>
2023-06-03 19:04:42 +0300

    msg

---
+++ GroupProject/TicTacToe/Model/StaticUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserModel;
+
+namespace Client.Model
+{
+    internal static class StaticUser
+    {
+		private static User user;
+
+		public static User User
+		{
+			get { return user; }
+			set { user = value; }
+		}
+
+	}
+}
